5bd73ecf34af7d9e99fc8a4a383e77f056655c84
ismaelhtmljs <ismaelgq.88@gmail.com>
2024-11-30 22:29:56 -0500

    new section and new css

--- src/layout/main.jsx
+++ src/layout/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import '../index.css'
 import '../index-contacto-section.css'
+import '../index-about-section.css'
 import Index from '../router/router'
 
 
--- src/pages/about-section.tsx
+++ src/pages/about-section.tsx
@@ -9,8 +9,9 @@ function About() {
                         <div className="contenedor-contenido-post-about">
                             <div className="post1-about">
                                 <section className="subtitulo-post1-about">
-                                    <h2></h2>
+                                    <h2>Sobre <em>Project IDX</em></h2>
                                 </section>
+                                <p>Project IDX es un editor de código abierto que permite a los desarrolladores crear contenido de alta calidad y alta velocidad. Construido para simplificar el proceso de creación y distribución de contenido, Project IDX te permite crear, editar, y publicar contenido en la web de manera rápida y fácil.</p>
                             </div>
                         </div>
                     </div>
